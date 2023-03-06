40a4fd1352421ff853562ee6c6a5e5ebedae9e82
Filip Parag <filip@parag.rs>
2023-03-06 22:32:19 +0100

    inode implementation start

--- src/structs.rs
+++ src/structs.rs
@@ -1,14 +1,34 @@
 #![allow(unused)]
 
 mod bitmap;
+mod inode;
 mod superblock;
 
-pub use bitmap::*;
-pub use superblock::*;
+use std::{
+    io::{Read, Seek, Write},
+    marker::PhantomData,
+};
 
-const BITMAP_WORD: u64 = 128;
+use fuser::FileType;
+
+const BITMAP_WORD: u64 = std::mem::size_of::<usize>() as u64;
+const BITMAP_MIN_SIZE: u64 = 1024;
 const DATA_PER_INODE: u64 = 4096;
 const MAGIC_SIGNATURE: u16 = 0xEF53;
+
+pub(crate) trait PermanentIndexed: Sized {
+    type Error;
+    fn load<D: Read + Seek>(
+        block_device: &mut D,
+        superblock: &Superblock,
+        index: u64,
+    ) -> Result<Self, Self::Error>;
+    fn flush<D: Write + Seek>(
+        &self,
+        block_device: &mut D,
+        superblock: &Superblock,
+    ) -> Result<(), Self::Error>;
+}
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
@@ -22,9 +42,9 @@ pub struct Superblock {
     /// Count of free blocks in the filesystem
     pub(crate) blocks_free: u64,
     /// Block size in bytes
-    pub(crate) block_size: u64,
+    pub(crate) block_size: u32,
     #[doc(hidden)]
-    __padding_1: [u8; 16],
+    __padding_1: [u8; 20],
     /// Magic signature
     pub(crate) magic: u16,
     #[doc(hidden)]
@@ -34,24 +54,26 @@ pub struct Superblock {
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
 pub struct Inode {
+    /// Inode's index
+    pub(crate) index: u64,
     /// File mode (permissions)
     pub(crate) mode: u16,
     /// File type
-    pub(crate) r#type: u8,
+    pub(crate) r#type: FileType,
     /// File size in bytes
     pub(crate) size: u64,
     /// Owner UID
-    pub(crate) uid: u16,
+    pub(crate) uid: u32,
     /// Owner GID
-    pub(crate) gid: u16,
+    pub(crate) gid: u32,
     /// Last access timestamp in seconds
-    pub(crate) atime: i64,
-    /// Creation timestamp in seconds
-    pub(crate) ctime: i64,
-    /// Last modification timestamp in seconds
-    pub(crate) mtime: i64,
-    /// Deletion timestamp in seconds (zero if undeleted)
-    pub(crate) dtime: i64,
+    pub(crate) atime: u64,
+    /// Last metadata modification timestamp in seconds
+    pub(crate) ctime: u64,
+    /// Last data modification timestamp in seconds
+    pub(crate) mtime: u64,
+    /// Deletion timestamp in seconds ([`u64::MAX`](core::u64::MAX) if not deleted)
+    pub(crate) dtime: u64,
     /// Occupied block count
     pub(crate) block_count: u64,
     /// File is stored in contiguous blocks
@@ -63,16 +85,30 @@ pub struct Inode {
     /// block [`blocks_extra`](Self::blocks_extra)
     pub(crate) extent_sequence: bool,
     /// Beginning of sequence of blocks containing file's data
-    pub(crate) blocks: [u64; 7],
+    pub(crate) blocks: [u64; 6],
     #[doc(hidden)]
-    __padding_1: [bool; 7],
+    __padding_1: [bool; 3],
     /// Block containing continuation of block sequence if
     /// [`extent_sequence`](Self::extent_sequence) is `true`
     pub(crate) blocks_extra: u64,
 }
 
 #[derive(Debug, Clone)]
-pub struct Bitmap {
-    bitfield: Vec<u128>,
-    count: u64,
+pub struct Block {
+    /// Block's index
+    pub(crate) index: u64,
+    /// Raw data as bytes
+    pub(crate) data: Vec<u8>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Bitmap<T> {
+    /// Bits mapping to indexes
+    pub bitfield: Vec<usize>,
+    /// Number of valid indexes
+    pub count: u64,
+    /// Position
+    pub position: u64,
+    #[doc(hidden)]
+    __type: PhantomData<T>,
 }
---
+++ src/structs/inode.rs
@@ -0,0 +1,85 @@
+use super::*;
+
+use fuser::{FileAttr, FileType};
+use std::{
+    io::{Read, Seek, SeekFrom, Write},
+    time::{Duration, UNIX_EPOCH},
+};
+
+impl Inode {
+    pub fn attrs(&self, superblock: &Superblock) -> FileAttr {
+        FileAttr {
+            ino: self.index,
+            size: self.size,
+            blocks: self.block_count,
+            atime: UNIX_EPOCH + Duration::from_secs(self.atime),
+            mtime: UNIX_EPOCH + Duration::from_secs(self.mtime),
+            ctime: UNIX_EPOCH + Duration::from_secs(self.ctime),
+            crtime: UNIX_EPOCH + Duration::from_secs(self.ctime),
+            kind: FileType::BlockDevice,
+            perm: self.mode,
+            nlink: 0, // unimplemented
+            uid: self.uid,
+            gid: self.gid,
+            rdev: 0, // unimplemented
+            blksize: superblock.block_size as u32,
+            flags: 0, // unimplemented
+        }
+    }
+}
+
+impl PermanentIndexed for Inode {
+    type Error = crate::Error;
+
+    fn load<D: Read + Seek>(
+        block_device: &mut D,
+        superblock: &Superblock,
+        index: u64,
+    ) -> Result<Self, Self::Error> {
+        let position = superblock.inode_position(index)?;
+        block_device.seek(SeekFrom::Start(position))?;
+        let mut inode_raw = [0u8; std::mem::size_of::<Self>() / std::mem::size_of::<u8>()];
+        block_device.read_exact(&mut inode_raw)?;
+        Ok(unsafe { *(inode_raw.as_ptr() as *const Self) })
+    }
+
+    fn flush<D: Write + Seek>(
+        &self,
+        block_device: &mut D,
+        superblock: &Superblock,
+    ) -> Result<(), Self::Error> {
+        let position = superblock.inode_position(self.index)?;
+        block_device.seek(SeekFrom::Start(position))?;
+        let inode_raw = unsafe {
+            core::slice::from_raw_parts(
+                self as *const Self as *const u8,
+                std::mem::size_of::<Self>(),
+            )
+        };
+        block_device.write_all(inode_raw)?;
+        Ok(())
+    }
+}
+
+impl Default for Inode {
+    fn default() -> Self {
+        Self {
+            index: 0,
+            mode: 0,
+            r#type: FileType::RegularFile,
+            size: 0,
+            uid: 0,
+            gid: 0,
+            atime: 0,
+            ctime: 0,
+            mtime: 0,
+            dtime: 0,
+            block_count: 0,
+            block_range: false,
+            extent_sequence: false,
+            blocks: [0; 6],
+            __padding_1: Default::default(),
+            blocks_extra: 0,
+        }
+    }
+}
